9f02c03f33a25f486192bd908c8b9e5ea18bff52
Saray <saraybakker@gmail.com>
2025-04-17 17:15:50 +0200

    hierarchical R3S3 not working, but all examples run

--- examples/kinova_TamedPUMA_R3S3_hierarchical.py
+++ examples/kinova_TamedPUMA_R3S3_hierarchical.py
@@ -52,7 +52,6 @@ class example_kinova_tamedpuma_R3S3_hierarchical(ExampleGeneric):
             self.initialize_environment()
 
         # Construct classes:
-        # results_base_directory = '../pumafabrics/puma_adapted/'
         self.kuka_kinematics = KinematicsKuka(dt=self.params["dt"],
                                               robot_name=self.params["robot_name"],
                                               root_link_name=self.params["root_link"],
@@ -73,7 +72,7 @@ class example_kinova_tamedpuma_R3S3_hierarchical(ExampleGeneric):
             self.params_name = self.params["params_name_2nd"]
 
         # Load parameters
-        Params = getattr(importlib.import_module('pumafabrics.puma_adapted.params.' + self.params_name), 'Params')
+        Params = getattr(importlib.import_module('pumafabrics.puma_extension.params.' + self.params_name), 'Params')
         params = Params(results_base_directory)
         params.results_path += params.selected_primitives_ids + '/'
         params.load_model = True
--- pumafabrics/puma_extension/train.py
+++ pumafabrics/puma_extension/train.py
@@ -5,7 +5,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 # Get arguments
 parser = ArgumentParser()
-parser.add_argument('--params', type=str, default='2nd_order_R3S3_tomato_31may', help='')
+parser.add_argument('--params', type=str, default='2nd_order_R3S3_kinova', help='')
 parser.add_argument('--results-base-directory', type=str, default='./', help='')
 args = parser.parse_args()
 
